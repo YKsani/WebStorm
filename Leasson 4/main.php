4f54a75397971d7e82f201b4c2e338dae860c302
YKsani <yan-kowalski@mail.ru>
2022-12-23 22:11:40 +0500

    Leasson - 4

---
+++ Leasson 4/main.php
@@ -0,0 +1,28 @@
+<?php
+$name = 'Ян';
+$city = 'Санкт-Петербург, Россия';
+$profi = 'Программист';
+$email = 'email@email.com';
+$phone = 89777777777;
+$age = 2022-1996;
+$days = (2022-1996)*365;
+
+$skills = [
+        'skill_name' => ['PyCharm', 'WebStorm', 'Visual Studio Code', 'Visual Studio 2022'],
+        'level' => [60, 44, 62, 63]
+    ];
+
+$expirience = [[
+        'position' => 'Tester',
+        'period' => 'Oct 2022 - ',
+        'description' => 'Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure,iste.'
+       ],
+       ['position' => 'Programmer / test.com',
+        'period' => 'Jan 2022 - ',
+        'description' => 'Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure, iste.'
+       ],
+       ['position' => 'White hacker / test.com',
+        'period' => 'Sep 2022 - ',
+        'description' => 'Lorem ipsum dolor sit amet. Praesentium magnam consectetur vel in deserunt aspernatur est reprehenderit sunt hic. Nulla tempora soluta ea et odio, unde doloremque repellendus iure,iste.']
+    ];
+?>
